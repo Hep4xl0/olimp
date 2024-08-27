2a1e95bc94d8a98a8f364b20b39dbdfe86e7a2d4
Hep4xl0 <henryepaulo@outlook.com>
2024-08-27 14:18:41 -0300

    att

--- front-end/olympics-museum/src/components/City.jsx
+++ front-end/olympics-museum/src/components/City.jsx
@@ -1,6 +1,137 @@
 import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 
+import styled from 'styled-components'
+import { useRef, useEffect } from 'react';
+import gold from '../images/gold.svg'
+import silver from '../images/silver.svg'
+import bronze from '../images/bronze.svg'
+import all from '../images/all.svg'
+
+const MainBox = styled.div`
+    width: 500px;
+    height: 600px;
+`
+const YearBox = styled.div`
+    cursor: grab;
+    border: solid 2px white;
+    background-color: #181818;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    overflow: auto;
+    white-space: nowrap;
+    scroll-behavior: smooth;
+    scroll-snap-type: x mandatory;
+`
+const Year = styled.button`
+    cursor: pointer;
+    user-select: none;
+    color: white;
+    font-family: 'Montserrat', sans-serif;
+    font-weight: 600;
+    font-size: 2.5rem;
+    text-align: center;
+    border: none;
+    background: none;
+    margin: 0;
+    padding: 20px;
+    transition: 200ms ease;
+    opacity: 0.5;
+    &:hover {
+        opacity: 1;
+    }
+`
+const Score = styled.div`
+    user-select: none;
+    height: 500px;
+    background-color: #181818;
+    overflow: auto;
+`
+const CityName = styled.h2`
+    margin: 0;
+    padding: 20px;
+    color: white;
+    font-family: montserrat;
+    font-size: 2rem;
+    font-style: italic;
+    font-weight: 500;
+    text-align: center;
+`
+const Positions = styled.div`
+    background-color: #202020;
+    margin: 0px 10px;
+    padding: 15px;
+    display: flex;
+    flex-direction: column;
+`
+const TopDescription = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 5px 0px;
+`
+const IconMedals = styled.div`
+`
+const Description = styled.p`
+    color: grey;
+    margin: 0;
+    font-family: montserrat;
+    padding-bottom: 10px;
+`
+const Img = styled.img`
+    width: 20px;
+    height: 20px;
+    padding-right: 10px;
+`
+const Classification = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    border-top: solid 1px grey; 
+    padding: 5px 10px;
+`
+const Position = styled.p`
+    margin: 0;
+    color: white;
+    font-family: arial;
+`
+const Country = styled.h3`
+    color: white;
+    margin: 0;
+    font-family: montserrat;
+    font-size: 1rem;
+    font-weight: 500;
+    padding: 10px 0px;
+`
+const Results = styled.div`
+    display: flex;
+    gap: 10px;
+    font-size: 1.12rem;
+`
+const GoldMedal = styled.p`
+    margin: 0;
+    color: white;
+    font-family: montserrat;
+`
+const SilverMedal = styled.p`
+    margin: 0;
+    color: white;
+    font-family: montserrat;
+`
+const BronzeMedal = styled.p`
+    margin: 0;
+    color: white;
+    font-family: montserrat;
+`
+const TotalMedal = styled.p`
+    margin: 0;
+    color: white;
+    font-family: montserrat;
+`
+
 function City() {
     const trackRef = useRef(null);
     const [dates, setDates] = useState([]);
@@ -46,43 +177,46 @@ function City() {
     }, [selectedYear]);
 
     return (
-        <div className="main-box">
-            <div className="year-box" ref={trackRef}>
+        <MainBox>
+            <YearBox ref={trackRef}>
                 {dates.map((date) => (
-                    <div 
+                    <Year 
                         key={date} 
                         className="year" 
                         onClick={() => setSelectedYear(date)}
                     >
                         {date}
-                    </div>
+                    </Year>
                 ))}
-            </div>
-            <div className="score">
-                <h1 className="city-name">PARIS</h1>
-                <div className="positions">
-                    <div className="top-description">
-                        <span className="description">Team</span>
-                        <div className="icon-medals">
-                           
-                        </div>
-                    </div>
+            </YearBox>
+            <Score>
+                <CityName>PARIS</CityName>
+                <Positions>
+                    <TopDescription>
+                        <Description>Team</Description>
+                        <IconMedals>
+                           <Img scr={gold}></Img>
+                           <Img scr={silver}></Img>
+                           <Img scr={bronze}></Img>
+                           <Img scr={all}></Img>
+                        </IconMedals>
+                    </TopDescription>
                     {/* Renderiza os dados de medalhas */}
                     {Object.keys(medals).map((country, index) => (
-                        <div className="classification" key={index}>
-                            <div className="position">{index + 1}</div>
-                            <div className="country">{country}</div>
-                            <div className="results">
-                                <span className="gold-medal">{medals[country].gold}</span>
-                                <span className="silver-medal">{medals[country].silver}</span>
-                                <span className="bronze-medal">{medals[country].bronze}</span>
-                                <span className="total-medal">{medals[country].total}</span>
-                            </div>
-                        </div>
+                        <Classification key={index}>
+                            <Position>{index + 1}</Position>
+                            <Country>{country}</Country>
+                            <Results>
+                                <GoldMedal>{medals[country].gold}</GoldMedal>
+                                <SilverMedal>{medals[country].silver}</SilverMedal>
+                                <BronzeMedal>{medals[country].bronze}</BronzeMedal>
+                                <TotalMedal>{medals[country].total}</TotalMedal>
+                            </Results>
+                        </Classification>
                     ))}
-                </div>
-            </div>
-        </div>
+                </Positions>
+            </Score>
+        </MainBox>
     );
 }
 
--- front-end/olympics-museum/src/components/Page.jsx
+++ front-end/olympics-museum/src/components/Page.jsx
@@ -3,6 +3,72 @@ import axios from 'axios';
 import City from './City';
 import Season from './Season';
 
+const Header = styled.header`
+    background: linear-gradient(90deg, #0040E5, #2D0051);
+    display: flex;
+    padding: 35px;
+    align-items: center;
+    justify-content: space-between;
+`
+const Title = styled.h1`
+    user-select: none;
+    margin: 0;
+    color: white;
+    font-size: 2.5rem;
+    font-family: montserrat;
+    font-weight: 550;
+`
+const Logo = styled.img`
+    width: 105px;
+    height: 45px;
+`
+const Footer = styled.footer`
+background-color: #181818;
+    display: flex;
+    flex-direction: column;
+    padding: 35px;
+    align-items: center;
+`
+const Websites = styled.div`
+    display: flex;
+    gap: 100px;
+`
+const Names = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+const SubTitle = styled.h2`
+    user-select: none;
+    margin-bottom: 10px;
+    color: white;
+    font-size: 2rem;
+    font-family: montserrat;
+    font-weight: 450;
+`
+const Link = styled.a`
+    color: white;
+    margin: 5px 0px;
+    font-family: montserrat;
+    font-weight: 400;
+    text-decoration-line: none;
+    opacity: 0.5;
+    transition: 200ms ease;
+    &:hover {
+        opacity: 1;
+    }
+    &:active {
+        scale: 95%;
+    }
+`
+const Windows = styled.div`
+    display: flex;
+    flex-direction: row;
+    padding: 40px;
+    gap: 45px;
+    justify-content: center;
+`
+
 function Page() {
     const [years, setYears] = useState([]);
     const [selectedYear, setSelectedYear] = useState("2021");  // Ano padrão
@@ -22,24 +88,12 @@ function Page() {
 
     return (
         <>
-            <header style={{
-                display: 'flex',
-                justifyContent: 'space-between',
-                alignItems: 'center',
-                padding: '20px',
-                backgroundColor: '#f8f9fa'
-            }}>
-                <h1 style={{ margin: 0 }}>OLYMPICS MUSEUM</h1>
-               
-            </header>
+            <Header>
+                <Title>OLYMPICS MUSEUM</Title>
+                <Logo src={OlympucsImage} alt='Olympics Image'/>
+            </Header>
 
-            <div style={{
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                padding: '20px',
-                backgroundColor: '#e9ecef'
-            }}>
+            <Windows>
                 {/* Passando os anos e o ano selecionado para o componente City */}
                 <City 
                     years={years} 
@@ -54,35 +108,23 @@ function Page() {
                     onSeasonChange={setSeason}
                     onCountryChange={setSelectedCountry}
                 />
-            </div>
+            </Windows>
             
-            <footer style={{
-                padding: '20px',
-                backgroundColor: '#343a40',
-                color: '#ffffff',
-                textAlign: 'center'
-            }}>
-                <h1 style={{ margin: 0 }}>CREDITS</h1>
-                <div style={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    marginTop: '10px'
-                }}>
-                    <div style={{ marginBottom: '10px' }}>
-                        <h2 style={{ margin: '5px' }}>GitHub</h2>
-                        <a href="https://github.com/Hep4xl0" style={{ color: '#ffffff', textDecoration: 'none' }}>@Hep4xl0</a>
-                        <br />
-                        <a href="https://github.com/Z4ffarani" style={{ color: '#ffffff', textDecoration: 'none' }}>@Z4ffarani</a>
-                    </div>
-                    <div>
-                        <h2 style={{ margin: '5px' }}>LinkedIn</h2>
-                        <a href="https://www.linkedin.com/in/hepp4xl0" style={{ color: '#ffffff', textDecoration: 'none' }}>Henry Paulo</a>
-                        <br />
-                        <a href="https://www.linkedin.com/in/kaique-zaffarani" style={{ color: '#ffffff', textDecoration: 'none' }}>Kaique Zaffarani</a>
-                    </div>
-                </div>
-            </footer>
+            <Footer>
+                <Title>CREDITS</Title>
+                <Websites>
+                    <Names>
+                        <SubTitle>GitHub</SubTitle>
+                        <Link href="https://github.com/Hep4xl0" style={{ color: '#ffffff', textDecoration: 'none' }}>@Hep4xl0</Link>
+                        <Link href="https://github.com/Z4ffarani" style={{ color: '#ffffff', textDecoration: 'none' }}>@Z4ffarani</Link>
+                    </Names>
+                    <Names>
+                        <SubTitle>LinkedIn</SubTitle>
+                        <Link href="https://www.linkedin.com/in/hepp4xl0" style={{ color: '#ffffff', textDecoration: 'none' }}>Henry Paulo</Link>
+                        <Link href="https://www.linkedin.com/in/kaique-zaffarani" style={{ color: '#ffffff', textDecoration: 'none' }}>Kaique Zaffarani</Link>
+                    </Names>
+                </Websites>
+            </Footer>
         </>
     );
 }
